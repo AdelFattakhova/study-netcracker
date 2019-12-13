9b1bde99bae1ef0c2645416de3fd0f7c65bcdce3
Adel Fattakhova <adel.fattakhova.162@gmail.com>
2019-12-13 20:29:26 +0300

    binary search corrected

--- learn-js/binary.js
+++ learn-js/binary.js
@@ -1,27 +1,33 @@
-let binarySearch = function(arr, num) {
+/**
+ * Функция бинарного поиска
+ *
+ * @param arr - упорядоченный массив целых чисел
+ * @param num - число, индекс которого надо найти в массиве
+ * @returns {any} - найденный индекс числа либо null, если число не найдено
+ */
+
+const binarySearch = (arr, num) => {
+
     let left = 0;
     let right = arr.length - 1;
+    let middle = Math.floor((left + right) / 2);
 
     while (left <= right) {
-        let middle = Math.floor((left + right) / 2);
-        let middleNum = arr[middle];
-
-        if(num == middleNum) {
-            return middle;
-        }
-
-        else if (num < middleNum) {
+        if (num < arr[middle]) {
             right = middle - 1;
         } else {
             left = middle + 1;
         }
 
+        middle = Math.floor((left + right) / 2);
     }
-    return "Число не найдено :(";
-}
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let num = prompt("Введите число:");
+    return num === arr[middle] ? middle : null;
+};
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const num = prompt("Введите число:");
 
 let result = binarySearch(arr, num);
+result === null ? result = "Число не найдено :(" : result;
 console.log(result);
