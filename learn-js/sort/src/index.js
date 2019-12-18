c306fe45213e321b00c920915d633f60a88d01da
Adel Fattakhova <adel.fattakhova.162@gmail.com>
2019-12-18 22:52:38 +0300

    correct solution

--- learn-js/sort/src/index.js
+++ learn-js/sort/src/index.js
@@ -4,22 +4,16 @@
  * This function sorts letters in words and words in sentence in alphabetical order,
  * words in sentence are written with the first capital letter
  *
- * @param sentence - sentence to sort
+ * @param {string} sentence - sentence to sort
  * @returns {string} - sorted sentence
  */
 
-const sort = (sentence) => {
-    let words = sentence.split(' ')
-        .map(word => word.split('')
-            .sort((a, b) => {
-                return a.localeCompare(b, 'ru')
-            })
-            .map(letter => letter.toLowerCase())
-            .join(''))
-        .map(item => item[0].toUpperCase() + item.slice(1))
-        .sort((a, b) => {
-            return a.localeCompare(b, 'ru')
-        });
-
-    return words.join(' ');
-};
+const sort = sentence => sentence
+    .split(' ')
+    .map(word => word.split('')
+        .sort((a, b) => a.localeCompare(b, 'ru'))
+        .map(letter => letter.toLowerCase())
+        .join(''))
+    .map(item => `${item[0].toUpperCase()}${item.slice(1)}`)
+    .sort((a, b) => a.localeCompare(b, 'ru'))
+    .join(' ');
