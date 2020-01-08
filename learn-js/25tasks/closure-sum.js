ebb7cd598204617d3a05a330591d87f2b0022504
Adel Fattakhova <adel.fattakhova.162@gmail.com>
2020-01-08 09:46:30 +0300

    add foo function for currying

--- learn-js/25tasks/closure-sum.js
+++ learn-js/25tasks/closure-sum.js
@@ -5,3 +5,22 @@ const sum = a => b => a + b;
 
 console.log(sum(1)(2));
 console.log(sum(5)(-1));
+
+const foo = func => {
+    return function curry(...args) {
+        if (func.length <= args.length) {
+            return func.apply(this, args);
+        } else {
+            return (...moreArgs) => curry.apply(this, args.concat(moreArgs));
+        }
+    }
+};
+
+const sum4 = (a, b, c, d) => a + b + c + d;
+const f = foo(sum4);
+console.log(f(1, 2, 3)(4)); // 10
+console.log(f(1, 2)(3)(4)); // 10
+console.log(f(1)(2)(3)(4));// 10
+console.log(f(1)(2, 3, 4));// 10
+console.log(f(1)(2)(3, 4));// 10
+console.log(f(1, 2)(3, 4));// 10
